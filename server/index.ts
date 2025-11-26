597d5eeb4735087a6e40d271466df469b7a64171
u6875270078 <u6875270078@gmail.com>
2025-11-26 10:19:53 +0100

    Initial commit

--- server/index.ts
+++ server/index.ts
@@ -155,11 +155,12 @@ app.use((req, res, next) => {
     // this serves both the API and the client.
     // It is the only port that is not firewalled.
     const port = parseInt(process.env.PORT || '5000', 10);
+    const host = process.env.NODE_ENV === 'development' ? 'localhost' : '0.0.0.0';
     
     server.listen({
       port,
-      host: "0.0.0.0",
-      reusePort: true,
+      host,
+      reusePort: process.platform !== 'win32',
     }, () => {
       log(`serving on port ${port}`);
       log(`Environment: ${app.get("env")}`, 'express');
